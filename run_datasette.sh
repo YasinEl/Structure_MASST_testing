e3f917f4cd00b9b7724d76042e7f2ae6869e4316
Mingxun Wang <mwang87@gmail.com>
2025-08-01 11:16:42 -0700

    updating

--- run_datasette.sh
+++ run_datasette.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 datasette -p 5234 -h 0.0.0.0 \
---setting base_url /datasette/ \
 --setting max_returned_rows 50000 \
 --setting sql_time_limit_ms 60000 \
 --setting max_csv_mb 0 /app/database/masst_records_copy.sqlite
