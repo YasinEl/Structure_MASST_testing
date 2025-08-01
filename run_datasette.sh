d7e8d18763d723a3bcca3e75b62034a4abd290f5
Mingxun Wang <mwang87@gmail.com>
2025-08-01 11:21:52 -0700

    immutable

--- run_datasette.sh
+++ run_datasette.sh
@@ -3,4 +3,4 @@
 datasette -p 5234 -h 0.0.0.0 \
 --setting max_returned_rows 50000 \
 --setting sql_time_limit_ms 60000 \
---setting max_csv_mb 0 /app/database/masst_records_copy.sqlite
+--setting max_csv_mb 0 --immutable /app/database/masst_records_copy.sqlite
